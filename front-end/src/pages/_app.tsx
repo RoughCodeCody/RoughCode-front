02580265822e23ff71a49d2fdc6133d3094e9268
유정훈 <choanury@naver.com>
2023-04-19 14:03:22 +0900

    chore(FE): #S08P31A306-96 set global styles

--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "@/styles/global-styles";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default App;
