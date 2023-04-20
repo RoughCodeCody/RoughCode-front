38ebd514c0d4157eabe0aeb9f8c7de80bcd967e9
hani2057 <hanikim2057@gmail.com>
2023-04-21 00:47:19 +0900

    feat(FE): #S08P31A306-80 make portfolio notice component

---
+++ front-end/src/features/portfolios/components/notice/style.ts
@@ -0,0 +1,55 @@
+import * as RadixNavMenu from "@radix-ui/react-navigation-menu";
+import styled from "styled-components";
+
+const RadixNavMenuRoot = styled(RadixNavMenu.Root)`
+  width: 15%;
+  position: relative;
+`;
+
+const RadixNavMenuItem = styled(RadixNavMenu.Item)`
+  ${({ theme }) => theme.MIXINS.flexBox()}
+`;
+
+const RadixNavMenuTrigger = styled(RadixNavMenu.Trigger)`
+  all: unset;
+`;
+
+const RadixNavMenuContent = styled(RadixNavMenu.Content)`
+  position: absolute;
+  top: calc(3.75rem - 1px);
+  left: 0;
+  width: 100%;
+  background-color: var(--sub-one-color);
+`;
+
+const VersionLink = styled.li`
+  padding: 1rem;
+  ${({ theme }) => theme.MIXINS.flexBox("row", "space-around")}
+
+  &:hover {
+    & span {
+      color: var(--main-color);
+    }
+  }
+`;
+
+const NoticeContent = styled.div`
+  width: 70%;
+  ${({ theme }) => theme.MIXINS.flexBox("row", "start")};
+`;
+
+const NoticeTime = styled.div`
+  width: 15%;
+  line-height: 1.2rem;
+  ${({ theme }) => theme.MIXINS.flexBox("column")}
+`;
+
+export {
+  RadixNavMenuRoot,
+  RadixNavMenuItem,
+  RadixNavMenuTrigger,
+  RadixNavMenuContent,
+  VersionLink,
+  NoticeContent,
+  NoticeTime,
+};
