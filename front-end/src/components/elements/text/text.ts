4e8f6ae2f99772f515b863aa6fb2d85d2e5f8ea1
hani2057 <hanikim2057@gmail.com>
2023-04-20 22:28:32 +0900

    chore(FE): #S08P31A306-80 make common component(Text)

---
+++ front-end/src/components/elements/text/text.ts
@@ -0,0 +1,15 @@
+import styled, { css } from "styled-components";
+
+export const Text = styled.span<{
+  size?: string;
+  color?: string;
+  bold?: boolean;
+  pointer?: boolean;
+}>`
+  ${({ size, color, bold, pointer }) => css`
+    font-size: ${size || "1rem"};
+    font-weight: ${bold ? "700" : "400"};
+    color: ${color ? "var(--" + color + "-color)" : "var(--font-color)"};
+    cursor: ${pointer ? "pointer" : "auto"};
+  `}
+`;
