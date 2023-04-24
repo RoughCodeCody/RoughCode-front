cf5749717b8b7746a619ccb176d13b7c281817f9
유정훈 <choanury@naver.com>
2023-04-24 13:00:31 +0900

    feat(FE): #S08P31A306-158 make a project card equipped with a horizontal scroll for tags

--- front-end/src/features/portfolios/components/portfolio-card/portfolio-card.tsx
+++ front-end/src/features/portfolios/components/portfolio-card/portfolio-card.tsx
@@ -1,37 +1,89 @@
+import { useState, useRef } from "react";
+
 import Image from "next/image";
 import { FlexDiv } from "@/components/elements";
 import { Text } from "@/components/elements";
+import {
+  ThumbnailContainer,
+  ImageContainer,
+  ProjectCardDescription,
+  TagContainer,
+} from "./style";
+import { TagChipSub } from "@/components/elements";
 
 export const PortfolioCard = () => {
+  const [isDown, setIsDown] = useState(false);
+  const [startX, setStartX] = useState<number | null>(null);
+  const [scrollLeft, setScrollLeft] = useState<number>(0);
+  const tagContainerRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    setIsDown(true);
+    if (tagContainerRef.current) {
+      setStartX(e.pageX - tagContainerRef.current.offsetLeft);
+      setScrollLeft(tagContainerRef.current.scrollLeft);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setIsDown(false);
+  };
+
+  const handleMouseUp = () => {
+    setIsDown(false);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!isDown) return;
+    e.preventDefault();
+    if (tagContainerRef.current && startX !== null) {
+      const x = e.pageX - tagContainerRef.current.offsetLeft;
+      const walk = x - startX;
+      tagContainerRef.current.scrollLeft = scrollLeft - walk;
+    }
+  };
+
+  const handleWheel = (event: React.WheelEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    if (tagContainerRef.current) {
+      tagContainerRef.current.scrollLeft += event.deltaY;
+    }
+  };
+
   return (
     <FlexDiv
       width="24rem"
-      height="32rem"
+      height="30rem"
       direction="column"
       border="solid 1px var(--shad-color)"
       radius="32px"
-      paddingY="2rem"
+      padding="2rem 0 1rem 0"
     >
-      <FlexDiv padding="0 1.2rem 2rem 1.2rem">
-        <Image
-          src="https://picsum.photos/400/300"
-          layout="responsive"
-          width={500}
-          height={300}
-          alt={"PJT"}
-        />
-      </FlexDiv>
+      <ThumbnailContainer>
+        <ImageContainer>
+          <Image
+            src="https://picsum.photos/400/300"
+            width={343.6}
+            height={234.4}
+            alt={"PJT"}
+            object-fit="cover"
+          />
+        </ImageContainer>
+      </ThumbnailContainer>
+
+      {/* 분리선 */}
       <FlexDiv
         width="100%"
         height="0"
         border="solid 1px var(--shad-color)"
       ></FlexDiv>
+
       <FlexDiv
         width="100%"
-        height="100%"
+        height="35%"
         paddingX="1.8rem"
         direction="column"
-        justify="start"
+        justify="center"
         align="start"
       >
         <FlexDiv paddingY="0.5rem">
@@ -39,13 +91,33 @@ export const PortfolioCard = () => {
             title
           </Text>
         </FlexDiv>
-        <FlexDiv width="100%" height="100%" direction="column" align="start">
-          <Text width="100%" height="60%" wrap="break-word">
-            descriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescript
-          </Text>
-          <Text width="100%" height="40%">
-            tag
-          </Text>
+        <FlexDiv
+          width="100%"
+          height="100%"
+          direction="column"
+          justify="space-between"
+          align="start"
+        >
+          <ProjectCardDescription>
+            descriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescriptdescript
+          </ProjectCardDescription>
+          <TagContainer
+            ref={tagContainerRef}
+            onMouseDown={handleMouseDown}
+            onMouseLeave={handleMouseLeave}
+            onMouseUp={handleMouseUp}
+            onMouseMove={handleMouseMove}
+            onWheel={handleWheel}
+          >
+            <TagChipSub tag="Javascript" />
+            <TagChipSub tag="Java" />
+            <TagChipSub tag="React" />
+            <TagChipSub tag="Spring" />
+            <TagChipSub tag="Javascript" />
+            <TagChipSub tag="Java" />
+            <TagChipSub tag="React" />
+            <TagChipSub tag="Spring" />
+          </TagContainer>
         </FlexDiv>
       </FlexDiv>
     </FlexDiv>
