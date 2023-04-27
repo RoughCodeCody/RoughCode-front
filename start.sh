83025068f664f046bcea347ee6fc3790d081e8bc
dongsu712 <dongsu712@naver.com>
2023-04-27 22:27:14 +0900

    fix(CM): #S08P31A306-176 fix start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ docker-compose -f docker-compose.yml pull
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml up --build -d
 
-docker rmi -f $(docker images -f "dangling=true" -q) || true
+docker rm -f $(docker ps -a -q --filter "status=exited") || true
