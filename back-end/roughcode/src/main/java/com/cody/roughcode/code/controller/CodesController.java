673a024518edb50bbee6b6df021dd55249dedc36
RyuJeongmin <jmyoo88@naver.com>
2023-04-28 13:49:10 +0900

    fix(BE): #S08P31A306-180 add CookieValue(required=false)

--- back-end/roughcode/src/main/java/com/cody/roughcode/code/controller/CodesController.java
+++ back-end/roughcode/src/main/java/com/cody/roughcode/code/controller/CodesController.java
@@ -46,7 +46,7 @@ public class CodesController {
 
     @Operation(summary = "코드 상세 조회 API")
     @GetMapping("/{codeId}")
-    ResponseEntity<?> getCode(@CookieValue(name = JwtProperties.ACCESS_TOKEN) String accessToken,
+    ResponseEntity<?> getCode(@CookieValue(name = JwtProperties.ACCESS_TOKEN, required = false) String accessToken,
                                  @Parameter(description = "코드 id 값", required = true) @PathVariable Long codeId) {
         Long userId = jwtTokenProvider.getId(accessToken);
 
