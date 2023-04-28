75403cb6456b3fa55d94be8ecb33a567e6a5b816
dongsu712 <dongsu712@naver.com>
2023-04-28 17:31:35 +0900

    feat(CM): #S08P31A306-178 split dev, deploy docker-compose

---
+++ start-deploy.sh
@@ -0,0 +1,5 @@
+docker-compose -f docker-compose.yml pull
+
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-deploy.yml up --build -d
+
+docker rm -f $(docker ps -a -q --filter "status=exited") || true
--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,5 @@
 docker-compose -f docker-compose.yml pull
 
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml up --build -d
+COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-dev.yml up --build -d
 
 docker rm -f $(docker ps -a -q --filter "status=exited") || true
