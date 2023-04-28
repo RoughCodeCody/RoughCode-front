2b27772bacbd745cd26e1d1cd9f64d5b36f97bfb
dongsu712 <dongsu712@naver.com>
2023-04-28 17:33:27 +0900

    feat(CM): #S08P31A306-178 fix dev, deploy docker-compose

--- start-deploy.sh
+++ start-deploy.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose.yml pull
+docker-compose -f docker-compose-deploy.yml pull
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-deploy.yml up --build -d
 
--- start-dev.sh
+++ start-dev.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose.yml pull
+docker-compose -f docker-compose-dev.yml pull
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-dev.yml up --build -d
 
