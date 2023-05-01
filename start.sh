3cbe20f67f3313ac3cd4160a9d6c57048bb05359
dongsu712 <dongsu712@naver.com>
2023-05-01 16:03:59 +0900

    feat(CM): #S08P31A306-277 fix start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 docker-compose -f docker-compose.yml pull
 
+./init-letsencrypt.sh
+
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml up --build -d
 
 docker rm -f $(docker ps -a -q --filter "status=exited") || true
