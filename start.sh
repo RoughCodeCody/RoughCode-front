269b59cc83c80a22bf3dbba2c29430a62f05ae0a
dongsu712 <dongsu712@naver.com>
2023-05-01 16:08:26 +0900

    feat(CM): #S08P31A306-277 switch location docker-compose down in start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 docker-compose -f docker-compose.yml pull
 
-./init-letsencrypt.sh
-
 docker-compose -f docker-compose.yml down
+
+
+./init-letsencrypt.sh
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml up --build -d
 
