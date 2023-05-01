0c2ab3f45c9a7a8793b4862085ec2834c5e8abbf
dongsu712 <dongsu712@naver.com>
2023-05-01 16:39:34 +0900

    feat(CM): #S08P31A306-277 change location init-letsencrypt.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,9 @@ docker-compose -f docker-compose.yml pull
 docker-compose -f docker-compose.yml down
 
 
-./init-letsencrypt.sh
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml up --build -d
+
+./init-letsencrypt.sh
 
 docker rm -f $(docker ps -a -q --filter "status=exited") || true
