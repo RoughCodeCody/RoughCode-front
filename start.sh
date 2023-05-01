7fb9d1bd157b6d3cad3cb3ba2f3e2541d69d1f3f
dongsu712 <dongsu712@naver.com>
2023-05-01 16:06:39 +0900

    feat(CM): #S08P31A306-277 add docker-compose down in start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@ docker-compose -f docker-compose.yml pull
 
 ./init-letsencrypt.sh
 
+docker-compose -f docker-compose.yml down
+
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml up --build -d
 
 docker rm -f $(docker ps -a -q --filter "status=exited") || true
