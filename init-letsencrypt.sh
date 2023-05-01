55b95fa0c650fb4212c429ae4b2216a91eb5bf54
dongsu712 <dongsu712@naver.com>
2023-05-01 15:56:24 +0900

    feat(CM): #S08P31A306-277 fix init-letsencrypt.sh domains

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="도메인 주소"
+domains=("rough-code.com")
 rsa_key_size=4096
 data_path="../certbot"
-email="이메일 주소" # Adding a valid address is strongly recommended
+email="dongsu712@naver.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
