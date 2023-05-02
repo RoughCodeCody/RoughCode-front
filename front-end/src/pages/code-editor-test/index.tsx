1c90da0db9691228c70c74b26e139177eea864b4
유정훈 <choanury@naver.com>
2023-05-02 18:03:44 +0900

    chore(FE): #S08P31A306-250 set monaco editor

---
+++ front-end/src/pages/code-editor-test/index.tsx
@@ -0,0 +1,11 @@
+import { CodeEditor } from "@/features/code-editor";
+
+const CodeEditorTest = () => {
+  return (
+    <>
+      <CodeEditor />
+    </>
+  );
+};
+
+export default CodeEditorTest;
