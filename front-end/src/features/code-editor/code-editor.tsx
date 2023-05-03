8b72b94920ad149bb5f7c1b46045575ec50287d5
유정훈 <eumyang99@gmail.com>
2023-05-04 07:36:00 +0900

    feat(FE): #S08P31A306-250 find overlap lines

--- front-end/src/features/code-editor/code-editor.tsx
+++ front-end/src/features/code-editor/code-editor.tsx
@@ -1,5 +1,24 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Editor, { OnMount } from "@monaco-editor/react";
+
+function isOverlap(a, b, c, d) {
+  // a와 b의 위치를 정렬합니다.
+  if (a > b) {
+    [a, b] = [b, a];
+  }
+
+  // c와 d의 위치를 정렬합니다.
+  if (c > d) {
+    [c, d] = [d, c];
+  }
+
+  // a~b 구간과 c~d 구간이 겹치는지 확인합니다.
+  if (b >= c && a <= d) {
+    return true; // 겹침
+  } else {
+    return false; // 겹치지 않음
+  }
+}
 
 export const CodeEditor: React.FC = () => {
   const editorRef = useRef<any>(null);
@@ -8,6 +27,8 @@ export const CodeEditor: React.FC = () => {
     null
   );
   const [selectionEndLine, setSelectionEndLine] = useState<number | null>(null);
+  const [selectedLines, setSelectedLines] = useState([]);
+  const [decorationsCollection, setDecorationsCollection] = useState([]);
 
   const handleEditorDidMount: OnMount = (editor, monaco) => {
     editorRef.current = editor;
@@ -23,23 +44,48 @@ export const CodeEditor: React.FC = () => {
     setSelectionEndLine(endLineNumber);
   };
 
-  const selectedLine = () => {
+  const colorizeSelectedLine = () => {
     if (monaco && selectionStartLine !== null && selectionEndLine !== null) {
-      const deltaDecorations = [
+      let deltaDecorations = [
         {
           range: new monaco.Range(selectionStartLine, 1, selectionEndLine, 1),
           options: { isWholeLine: true, className: "selected-line" },
         },
       ];
+      let newSelectedLines = [];
+      selectedLines.map((line) => {
+        if (
+          !isOverlap(line[0], line[1], selectionStartLine, selectionEndLine)
+        ) {
+          const deco = {
+            range: new monaco.Range(line[0], 1, line[1], 1),
+            options: { isWholeLine: true, className: "selected-line" },
+          };
+          deltaDecorations.push(deco);
+          newSelectedLines.push([line[0], line[1]]);
+        }
+      });
+      newSelectedLines.push([selectionStartLine, selectionEndLine]);
       editorRef.current?.deltaDecorations([], deltaDecorations);
+      setDecorationsCollection(deltaDecorations);
+      setSelectedLines(newSelectedLines);
     }
   };
 
+  const erase = () => {
+    console.log(editorRef.current.getLineDecorations());
+  };
+
+  useEffect(() => {
+    console.log(selectedLines);
+  }, [selectedLines]);
+
   return (
     <>
-      <button onClick={selectedLine}>색칠</button>
+      <button onClick={erase}>지우기</button>
+      <button onClick={colorizeSelectedLine}>색칠</button>
       <Editor
-        height="90vh"
+        height="60vh"
         defaultLanguage="javascript"
         defaultValue={`a = [1,2,3,4,5,]
 
--- front-end/src/pages/code-editor-test/index.tsx
+++ front-end/src/pages/code-editor-test/index.tsx
@@ -2,16 +2,30 @@ import React, { useRef } from "react";
 
 import { CodeEditor } from "@/features/code-editor";
 import { DiffCodeEditor } from "@/features/code-editor/diff-code-editor";
-
+import Tippy from "@tippyjs/react/headless";
 const CodeEditorTest = () => {
   return (
     <>
-      {/* <CodeEditor /> */}
-      <DiffCodeEditor
+      <CodeEditor />
+      {/* <DiffCodeEditor
         base64String={
           "IyDtm4Trs7TtgqQgCiMg7Jyg7J287ISxICYg7LWc7IaM7ISxIOunjOyhsQpmcm9tIGl0ZXJ0b29scyBpbXBvcnQgY29tYmluYXRpb25zCgpkZWYgc29sdXRpb24ocmVsYXRpb24pOgogICAgCiAgICByb3cgPSBsZW4ocmVsYXRpb24pCiAgICBjb2wgPSBsZW4ocmVsYXRpb25bMF0pCiAgICAKICAgICMg6rCA64ql7ZWcIOyGjeyEseydmCDrqqjrk6Ag7J24642x7IqkIOyhsO2VqQogICAgY2FzZSA9IFtdCiAgICBmb3IgaSBpbiByYW5nZSgxLCBjb2wrMSk6CiAgICAgICAgY2FzZS5leHRlbmQoY29tYmluYXRpb25zKHJhbmdlKGNvbCksIGkpKQo="
         }
-      />
+      /> */}
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <Tippy
+        render={(attrs) => (
+          <div className="box" tabIndex="-1" {...attrs}>
+            My tippy box
+          </div>
+        )}
+      >
+        <button>My button</button>
+      </Tippy>
     </>
   );
 };
