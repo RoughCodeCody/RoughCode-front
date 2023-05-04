53ff1ee81d4e18072b081b3f8e8d8d1a27884068
유정훈 <choanury@naver.com>
2023-05-04 18:09:01 +0900

    chore(FE): #S08P31A306-250 create code review feedback create and modify page

--- front-end/src/pages/code-editor-test/index.tsx
+++ front-end/src/pages/code-editor-test/index.tsx
@@ -1,25 +1,7 @@
-import React, { useRef } from "react";
-
-import { CodeEditor } from "@/features/code-editor";
-import { DiffCodeEditor } from "@/features/code-editor/diff-code-editor";
-import Tippy from "@tippyjs/react/headless";
-import { FlexDiv } from "@/components/elements";
+import { CreateFeedback } from "@/features/code-reviews";
 
 const CodeEditorTest = () => {
-  return (
-    <FlexDiv width="100vw" direction="column" gap="10rem">
-      <FlexDiv width="50%" height="50%">
-        <CodeEditor />
-      </FlexDiv>
-      <FlexDiv width="50%" height="50%">
-        <DiffCodeEditor
-          base64String={
-            "IyDtm4Trs7TtgqQgCiMg7Jyg7J287ISxICYg7LWc7IaM7ISxIOunjOyhsQpmcm9tIGl0ZXJ0b29scyBpbXBvcnQgY29tYmluYXRpb25zCgpkZWYgc29sdXRpb24ocmVsYXRpb24pOgogICAgCiAgICByb3cgPSBsZW4ocmVsYXRpb24pCiAgICBjb2wgPSBsZW4ocmVsYXRpb25bMF0pCiAgICAKICAgICMg6rCA64ql7ZWcIOyGjeyEseydmCDrqqjrk6Ag7J24642x7IqkIOyhsO2VqQogICAgY2FzZSA9IFtdCiAgICBmb3IgaSBpbiByYW5nZSgxLCBjb2wrMSk6CiAgICAgICAgY2FzZS5leHRlbmQoY29tYmluYXRpb25zKHJhbmdlKGNvbCksIGkpKQo="
-          }
-        />
-      </FlexDiv>
-    </FlexDiv>
-  );
+  return <CreateFeedback />;
 };
 
 export default CodeEditorTest;
