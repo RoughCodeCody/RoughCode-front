d01ae9f8fe3350d9a3d736c2ada760f028e6abcb
hani <hanikim2057@gmail.com>
2023-05-08 14:42:17 +0900

    chore(FE): #S08P31A306-211 fix conflict

--- front-end/src/features/code-reviews/pages/code-detail.tsx
+++ front-end/src/features/code-reviews/pages/code-detail.tsx
@@ -1,14 +1,23 @@
+import { FlexDiv, WhiteBoxNoshad } from "@/components/elements";
 import { CodeInfo } from "../components/code-info";
+import { useCodeInfo } from "../api/get-code-info";
 
 interface CodeDetailProps {
   codeId: string;
 }
 
 export const CodeDetail = ({ codeId }: CodeDetailProps) => {
-  console.log(codeId);
+  const codeInfoQuery = useCodeInfo(Number(codeId));
+
+  console.log(codeInfoQuery.data);
+
   return (
     <>
-      <CodeInfo />
+      <FlexDiv direction="column" gap="3rem" padding="2rem 0">
+        <WhiteBoxNoshad width="65%" padding="2.25rem">
+          {/* <CodeInfo /> */}
+        </WhiteBoxNoshad>
+      </FlexDiv>
     </>
   );
 };
