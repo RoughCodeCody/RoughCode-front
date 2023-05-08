b707a205a518df97560732c0a6984b3092a2262e
dongsu712 <dongsu712@naver.com>
2023-05-08 09:57:41 +0900

    fix(CM): #S08P31A306-306 fix .env in start.sh

--- start-dev.sh
+++ start-dev.sh
@@ -1,6 +1,5 @@
 docker-compose -f docker-compose-dev.yml pull
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-dev.yml up  --build -d
-# COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-dev.yml up --env-file=.env --build -d
 
 docker rm -f $(docker ps -a -q --filter "status=exited") || true
