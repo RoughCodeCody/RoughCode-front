a9d1322e41340876cd1ee2d4417998af1861e1cb
hani <hanikim2057@gmail.com>
2023-05-08 17:47:17 +0900

    style(FE): #S08P31A306-211 modify version info component to accommodate both project and code review

--- front-end/src/features/version-info/components/versions-info/versions-info.tsx
+++ front-end/src/features/version-info/components/versions-info/versions-info.tsx
@@ -9,32 +9,19 @@ type VersionsInfoProps = {
   curVersionId: string;
 };
 
+// 프로젝트 버전인지 여부를 판별
+function isProject(arg: any): arg is ProjectVersion[] {
+  return arg?.[0].projectId !== undefined;
+}
+
+// 코드 버전인지 여부를 판별
+function isCode(arg: any): arg is CodeVersion[] {
+  return arg?.[0].codeId !== undefined;
+}
+
 export const VersionsInfo = ({ versions, curVersionId }: VersionsInfoProps) => {
   // 더미데이터
   const isMine = true;
-  // const versions = [
-  //   {
-  //     version: "v2",
-  //     notice: "프로젝트 및 코드 즐겨찾기 기능이 추가되었습니다!",
-  //     current: true,
-  //     feedbacks: [
-  //       {
-  //         user: "닉네임",
-  //         content: "즐겨찾기 기능이 있으면 좋을 거 같아요",
-  //       },
-  //       {
-  //         user: "익명10",
-  //         content: "로그아웃 할 때 확인 한 번 해 주는 게 좋을 듯",
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     version: "v1",
-  //     notice: "개발새발 런칭! 프로젝트와 코드를 공유해보세요",
-  //     current: false,
-  //     feedbacks: [],
-  //   },
-  // ];
 
   return (
     <Accordion
@@ -42,17 +29,43 @@ export const VersionsInfo = ({ versions, curVersionId }: VersionsInfoProps) => {
       hasBtn={isMine}
       btnText="+ 새 버전 등록"
     >
-      <FlexDiv direction="column" gap="1rem">
-        {versions.map(({ version, notice, projectId, selectedFeedbacks }) => (
-          <VersionInfo
-            version={version}
-            notice={notice}
-            current={Boolean(projectId === parseInt(curVersionId))}
-            feedbacks={selectedFeedbacks}
-            key={projectId}
-          />
-        ))}
-      </FlexDiv>
+      {isProject(versions) ? (
+        <>
+          <FlexDiv direction="column" gap="1rem">
+            {versions.map(
+              ({ version, notice, projectId, selectedFeedbacks }) => (
+                <VersionInfo
+                  version={version}
+                  title={notice}
+                  current={Boolean(projectId === parseInt(curVersionId))}
+                  feedbacks={selectedFeedbacks}
+                  id={projectId}
+                  type="project"
+                  key={projectId}
+                />
+              )
+            )}
+          </FlexDiv>
+        </>
+      ) : isCode(versions) ? (
+        <>
+          <FlexDiv direction="column" gap="1rem">
+            {versions.map(({ version, codeId, selectedReviews }) => (
+              <VersionInfo
+                version={version}
+                title={"title"}
+                current={Boolean(codeId === parseInt(curVersionId))}
+                feedbacks={selectedReviews}
+                id={codeId}
+                type="code-review"
+                key={codeId}
+              />
+            ))}
+          </FlexDiv>
+        </>
+      ) : (
+        <></>
+      )}
     </Accordion>
   );
 };
