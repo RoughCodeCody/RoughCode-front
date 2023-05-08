a573757843056781683e7a753ae3789d3e0d31ea
dongsu712 <dongsu712@naver.com>
2023-05-08 13:22:14 +0900

    feat(CM): #S08P31A306-270 create sonarqube docker-compose.yml

--- start-dev.sh
+++ start-dev.sh
@@ -3,3 +3,5 @@ docker-compose -f docker-compose-dev.yml pull
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-dev.yml up  --build -d
 
 docker rm -f $(docker ps -a -q --filter "status=exited") || true
+
+docker-compose up -d
