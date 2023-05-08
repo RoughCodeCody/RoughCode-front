ba8c11b40e987cd90a1771174261ae890470e906
dongsu712 <dongsu712@naver.com>
2023-05-08 13:31:42 +0900

    fix(CM): #S08P31A306-270 fix start-dev sonar docker-compose

--- start-dev.sh
+++ start-dev.sh
@@ -2,6 +2,8 @@ docker-compose -f docker-compose-dev.yml pull
 
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose-dev.yml up  --build -d
 
-docker rm -f $(docker ps -a -q --filter "status=exited") || true
+# docker rm -f $(docker ps -a -q --filter "status=exited") || true
 
-docker-compose -f docker-compose.yml up -d --build
+docker ps -a -q --filter "status=exited" | xargs -r docker rm -f || true
+
+
