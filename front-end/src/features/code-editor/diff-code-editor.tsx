54306a266b6a9396a5ed08fcd1a21cf1d0b50827
유정훈 <choanury@naver.com>
2023-05-08 18:23:02 +0900

    chore(FE): #S08P31A306-259 send the modified code data from the DiffEditor component to the parent component

--- front-end/src/features/code-editor/diff-code-editor.tsx
+++ front-end/src/features/code-editor/diff-code-editor.tsx
@@ -15,82 +15,89 @@ interface DiffCodeEditorProps {
   readOnly?: boolean;
 }
 
-export const DiffCodeEditor: React.FC<DiffCodeEditorProps> = ({
-  headerText,
-  originalCode,
-  height,
-  language,
-  readOnly = true,
-}) => {
-  const diffEditorRef = useRef<any>(null);
-
-  function handleEditorDidMount(editor: any, monaco: any) {
-    diffEditorRef.current = editor;
-    if (readOnly) {
-      editor.updateOptions({ readOnly: true });
+export const DiffCodeEditor = React.forwardRef(
+  (
+    {
+      headerText,
+      originalCode,
+      height,
+      language,
+      readOnly = true,
+    }: DiffCodeEditorProps,
+    ref: React.ForwardedRef<any>
+  ) => {
+    function handleEditorDidMount(editor: any, monaco: any) {
+      if (ref) {
+        if (typeof ref === "function") {
+          ref(editor);
+        } else {
+          ref.current = editor;
+        }
+      }
+      if (readOnly) {
+        editor.updateOptions({ readOnly: true });
+      }
     }
+
+    // const decodeBase64ToUTF8 = (originalCode: string): string => {
+    //   const binaryString = atob(originalCode);
+    //   const bytes = new Uint8Array(binaryString.length);
+    //   for (let i = 0; i < binaryString.length; i++) {
+    //     bytes[i] = binaryString.charCodeAt(i);
+    //   }
+    //   const utf8String = new TextDecoder("utf-8").decode(bytes);
+    //   return utf8String;
+    // };
+
+    // const encodeUTF8ToBase64 = (code: string): string => {
+    //   const bytes = new TextEncoder().encode(code);
+    //   const originalCode = btoa(String.fromCharCode(...bytes));
+    //   return originalCode;
+    // };
+
+    const { Buffer } = require("buffer");
+
+    const decodeBase64ToUTF8 = (originalCode: string) => {
+      const padding = "=".repeat((4 - (originalCode.length % 4)) % 4);
+      const base64 = (originalCode + padding)
+        .replace(/-/g, "+")
+        .replace(/_/g, "/");
+      const rawData = Buffer.from(base64, "base64");
+      const utf8Decoder = new TextDecoder("utf-8");
+      return utf8Decoder.decode(rawData);
+    };
+
+    // const encodeUTF8ToBase64 = (code: string) => {
+    //   const utf8Encoder = new TextEncoder();
+    //   const data = utf8Encoder.encode(code);
+    //   const base64 = Buffer.from(data).toString("base64");
+    //   return base64.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+    // };
+    const decodedString = decodeBase64ToUTF8(originalCode);
+
+    // const postFeedback = () => {
+    //   const modifiedCode = ref?.getModifiedEditor().getValue();
+    //   const encodedCode = encodeUTF8ToBase64(modifiedCode);
+    //   // 이후 encodedCode를 api요청 보내는 코드
+    // };
+
+    return (
+      <EditorWrapper>
+        <EditorHeader>
+          <FlexDiv>
+            <FaRegLightbulb />
+            <Text padding="0 0 0 0.5rem">{headerText}</Text>
+          </FlexDiv>
+        </EditorHeader>
+        <DiffEditor
+          height={height}
+          language={language}
+          original={decodedString}
+          modified={decodedString}
+          onMount={handleEditorDidMount}
+        />
+        <EditorBottom />
+      </EditorWrapper>
+    );
   }
-
-  // const decodeBase64ToUTF8 = (originalCode: string): string => {
-  //   const binaryString = atob(originalCode);
-  //   const bytes = new Uint8Array(binaryString.length);
-  //   for (let i = 0; i < binaryString.length; i++) {
-  //     bytes[i] = binaryString.charCodeAt(i);
-  //   }
-  //   const utf8String = new TextDecoder("utf-8").decode(bytes);
-  //   return utf8String;
-  // };
-
-  // const encodeUTF8ToBase64 = (code: string): string => {
-  //   const bytes = new TextEncoder().encode(code);
-  //   const originalCode = btoa(String.fromCharCode(...bytes));
-  //   return originalCode;
-  // };
-
-  const { Buffer } = require("buffer");
-
-  const decodeBase64ToUTF8 = (originalCode: string) => {
-    const padding = "=".repeat((4 - (originalCode.length % 4)) % 4);
-    const base64 = (originalCode + padding)
-      .replace(/-/g, "+")
-      .replace(/_/g, "/");
-    const rawData = Buffer.from(base64, "base64");
-    const utf8Decoder = new TextDecoder("utf-8");
-    return utf8Decoder.decode(rawData);
-  };
-
-  const encodeUTF8ToBase64 = (code: string) => {
-    const utf8Encoder = new TextEncoder();
-    const data = utf8Encoder.encode(code);
-    const base64 = Buffer.from(data).toString("base64");
-    return base64.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
-  };
-  const decodedString = decodeBase64ToUTF8(originalCode);
-
-  const postFeedback = () => {
-    const modifiedCode = diffEditorRef?.current?.getModifiedEditor().getValue();
-    const encodedCode = encodeUTF8ToBase64(modifiedCode);
-    // 이후 encodedCode를 api요청 보내는 코드
-  };
-
-  useEffect(() => {}, [originalCode]);
-
-  return (
-    <EditorWrapper>
-      <EditorHeader>
-        <FlexDiv>
-          <FaRegLightbulb />
-          <Text padding="0 0 0 0.5rem">{headerText}</Text>
-        </FlexDiv>
-      </EditorHeader>
-      <DiffEditor
-        height={height}
-        language={language}
-        original={decodedString}
-        modified={decodedString}
-        onMount={handleEditorDidMount}
-      />
-      <EditorBottom />
-    </EditorWrapper>
-  );
-};
+);
--- front-end/src/features/code-reviews/pages/create-feedback.tsx
+++ front-end/src/features/code-reviews/pages/create-feedback.tsx
@@ -1,11 +1,9 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { useCodeInfo } from "../api/get-code-info";
 import { useCode } from "../api/get-code";
 import { CodeInfo } from "../components/code-info";
-import { FlexDiv, Title } from "@/components/elements";
-import { Text } from "@/components/elements";
-import { BottomHeader } from "@/components/elements";
+import { BottomHeader, Btn, FlexDiv, Title, Text } from "@/components/elements";
 import { CodeEditor, DiffCodeEditor } from "@/features/code-editor";
 
 type CreateFeedbackProps = {
@@ -13,15 +11,35 @@ type CreateFeedbackProps = {
 };
 
 export const CreateFeedback = ({ codeId }: CreateFeedbackProps) => {
+  const [coloredLines, setColoredLines] = useState<number[][]>([]);
+  const diffEditorRef = useRef<any>(null);
+
   // 여기서 정보 조회하고 하위 컴포넌트에 정보를 prop줌
   const codeInfoQuery = useCodeInfo(codeId);
-  console.log(codeInfoQuery.data);
 
   const githubUrl = codeInfoQuery.data?.githubUrl
     ? codeInfoQuery.data?.githubUrl
     : "";
   const codeQuery = useCode({ githubUrl });
   const originalCode = codeQuery.data?.content;
+
+  const encodeUTF8ToBase64 = (code: string) => {
+    const utf8Encoder = new TextEncoder();
+    const data = utf8Encoder.encode(code);
+    const base64 = Buffer.from(data).toString("base64");
+    return base64.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+  };
+  const postFeedback = () => {
+    const modifiedCode = diffEditorRef?.current?.getModifiedEditor().getValue();
+    // console.log(modifiedCode);
+    const encodedCode = encodeUTF8ToBase64(modifiedCode);
+    console.log(encodedCode);
+    // 이후 encodedCode를 api요청 보내는 코드
+  };
+
+  useEffect(() => {
+    console.log(coloredLines);
+  }, [coloredLines]);
 
   if (codeInfoQuery.isLoading) {
     return <div></div>;
@@ -54,6 +72,8 @@ export const CreateFeedback = ({ codeId }: CreateFeedbackProps) => {
             <>
               <FlexDiv width="100%" height="100%">
                 <CodeEditor
+                  coloredLines={coloredLines}
+                  setColoredLines={setColoredLines}
                   // 코드 라인 선택 기능 버튼 여부
                   headerText="피드백할 라인들을 드래그 한 후 선택 버튼을 눌러주세요"
                   lineSelection={true}
@@ -64,6 +84,7 @@ export const CreateFeedback = ({ codeId }: CreateFeedbackProps) => {
               </FlexDiv>
               <FlexDiv width="100%" height="100%">
                 <DiffCodeEditor
+                  ref={diffEditorRef}
                   headerText="코드를 수정해 주세요"
                   height="30rem"
                   readOnly={false}
@@ -91,6 +112,15 @@ export const CreateFeedback = ({ codeId }: CreateFeedbackProps) => {
         />
         {/* 여기가 md에디터 자리 */}
       </FlexDiv>
+
+      {/* 등록 버튼 : 서버에 post 요청*/}
+      <Btn
+        text={"등록"}
+        fontSize="2rem"
+        onClickFunc={() => {
+          postFeedback();
+        }}
+      />
     </FlexDiv>
   );
 };
