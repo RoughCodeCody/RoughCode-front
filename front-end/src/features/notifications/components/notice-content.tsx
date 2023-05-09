af1fd61069453be001ecba2b400621a72d5bb748
유정훈 <choanury@naver.com>
2023-05-09 14:33:10 +0900

    feat(FE): #S08P31A306-100 implement the feature to redirect to the corresponding post page when a notification is clicked

--- front-end/src/features/notifications/components/notice-content.tsx
+++ front-end/src/features/notifications/components/notice-content.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from "next/navigation";
+
 import { Notification } from "../types";
 import { FlexDiv, Text } from "@/components/elements";
 
@@ -8,15 +10,21 @@ type NoticeContentProps = {
 export const NoticeContent = ({
   data: { alarmId, section, content, postId, userId, createdDate },
 }: NoticeContentProps) => {
+  const router = useRouter();
   return (
     <FlexDiv>
       <Text size="1.3rem" bold={true}>
         {content[0]}{" "}
-        <Text as="span" size="1.3rem" bold={true} color="main" pointer={true}>
-          {/* {isProject ? "프로젝트 " : "코드리뷰 "} */}
+        <Text
+          as="span"
+          size="1.3rem"
+          bold={true}
+          color="main"
+          pointer={true}
+          onClick={() => router.push(`/${section}/${postId}`)}
+        >
           {content[1]}{" "}
           <Text as="span" size="1.3rem" bold={true}>
-            {/* {title} ver{version}{" "} */}
             {content[2]}{" "}
           </Text>
         </Text>
