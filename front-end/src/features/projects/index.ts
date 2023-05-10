db6e681ecbd71312ceb7d48fb5bb50cd4604a046
Joonha Yun <neverdcloud@gmail.com>
2023-05-10 09:23:20 +0900

    refactor(FE): update routes and indexes

--- front-end/src/features/projects/index.ts
+++ front-end/src/features/projects/index.ts
@@ -1,3 +1,5 @@
 export * from "./pages/project-detail";
+export * from "./pages/project-modify";
 export * from "./pages/project-register";
+export * from "./pages/project-upgrade";
 export * from "./pages/projects";
