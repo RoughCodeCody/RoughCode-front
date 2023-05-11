1db4ea0a459b2bdf597c55a6942ea6aa3775f340
hani <hanikim2057@gmail.com>
2023-05-11 12:13:37 +0900

    chore(FE): #S08P31A306-296 use character image for spinner

--- front-end/src/components/elements/index.ts
+++ front-end/src/components/elements/index.ts
@@ -8,6 +8,7 @@ export * from "./flexdiv";
 export * from "./modal";
 export * from "./nickname";
 export * from "./notice-alarm-item";
+export * from "./spinner";
 export * from "./tag-chip";
 export * from "./tag-chip-sub";
 export * from "./text";
