39d212487d1c635bea0b5db5fee68164e996934d
RyuJeongmin <jmyoo88@naver.com>
2023-05-15 14:44:29 +0900

    refactor(BE): #S08P31A306-346 add aop

--- back-end/roughcode/src/main/java/com/cody/roughcode/RoughcodeApplication.java
+++ back-end/roughcode/src/main/java/com/cody/roughcode/RoughcodeApplication.java
@@ -3,6 +3,7 @@ package com.cody.roughcode;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableBatchProcessing
 @EnableScheduling
+@EnableAspectJAutoProxy
 public class RoughcodeApplication {
 
     public static void main(String[] args) {
---
+++ back-end/roughcode/src/main/java/com/cody/roughcode/util/LoggingAspect.java
@@ -0,0 +1,57 @@
+package com.cody.roughcode.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    // com.cody.roughcode.*.controller 이하 패키지의 모든 클래스 이하 모든 메서드에 적용
+    @Pointcut("within(com.cody.roughcode..controller..*)")
+    public void myPointcut() { }
+
+    @Before("myPointcut()")
+    public void beforeParameterLog(JoinPoint joinPoint) {
+        // 메서드 정보 받아오기
+        Method method = getMethod(joinPoint);
+        log.info("======= 메서드 호출 전 = {} =======", method.getName());
+
+        // 파라미터 받아오기
+        Object[] args = joinPoint.getArgs();
+        if (args.length <= 0) log.info("no parameter");
+        for (Object arg : args) {
+            if(arg != null){
+                log.info("parameter type = {}", arg.getClass().getSimpleName());
+                log.info("parameter value = {}", arg);
+            }
+        }
+    }
+
+    // Poincut에 의해 필터링된 경로로 들어오는 경우 메서드 리턴 후에 적용
+    @AfterReturning(value = "myPointcut()", returning = "returnObj")
+    public void afterReturnLog(JoinPoint joinPoint, Object returnObj) {
+        // 메서드 정보 받아오기
+        Method method = getMethod(joinPoint);
+        log.info("======= 메서드 리턴 완료 = {} =======", method.getName());
+
+        log.info("return type = {}", returnObj.getClass().getSimpleName());
+        log.info("return value = {}", returnObj);
+    }
+
+    // JoinPoint로 메서드 정보 가져오기
+    private Method getMethod(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        return signature.getMethod();
+    }
+
+}
