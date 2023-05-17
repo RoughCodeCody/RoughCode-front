d305c2648cc22f6c30ee0bd024d9d139ab6df7d9
유정훈 <eumyang99@gmail.com>
2023-05-18 02:00:09 +0900

    chore(FE): #S08P31A306-347 add a mouse hover event to WhiteBoxShad

--- front-end/src/components/elements/white-box-shad/white-box-shad.ts
+++ front-end/src/components/elements/white-box-shad/white-box-shad.ts
@@ -1,5 +1,23 @@
 import styled, { css } from "styled-components";
 
+// export const WhiteBoxShad = styled.div<{
+//   width?: string;
+//   height?: string;
+//   radius?: string;
+//   shadColor?: string;
+//   padding?: string;
+//   margin?: string;
+// }>`
+//   ${({ width, height, radius, shadColor, padding, margin }) => css`
+//     width: ${width || "100%"};
+//     height: ${height || "100%"};
+//     background-color: var(--white-color);
+//     border-radius: ${radius || "25px"};
+//     box-shadow: 0 0 1.5rem -0.5rem var(--${shadColor || "shad"}-color);
+//     padding: ${padding || "0"};
+//     margin: ${margin || "0"};
+//   `}
+// `;
 export const WhiteBoxShad = styled.div<{
   width?: string;
   height?: string;
@@ -16,5 +34,10 @@ export const WhiteBoxShad = styled.div<{
     box-shadow: 0 0 1.5rem -0.5rem var(--${shadColor || "shad"}-color);
     padding: ${padding || "0"};
     margin: ${margin || "0"};
+    border: 1px solid transparent;
   `}
+
+  &:hover {
+    border: 1px solid var(--main-color);
+  }
 `;
