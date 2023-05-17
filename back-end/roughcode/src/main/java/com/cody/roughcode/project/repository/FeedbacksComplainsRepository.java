bc8400383dbe21c9e03bc37cd79e7b48390d5e02
kosy318 <77595685+kosy318@users.noreply.github.com>
2023-05-17 18:03:42 +0900

    refactor(BE): #S08P31A306-334 refactor complain apis

---
+++ back-end/roughcode/src/main/java/com/cody/roughcode/project/repository/FeedbacksComplainsRepository.java
@@ -0,0 +1,15 @@
+package com.cody.roughcode.project.repository;
+
+import com.cody.roughcode.project.entity.Feedbacks;
+import com.cody.roughcode.project.entity.FeedbacksComplains;
+import com.cody.roughcode.project.entity.FeedbacksLikes;
+import com.cody.roughcode.user.entity.Users;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FeedbacksComplainsRepository extends JpaRepository<FeedbacksComplains, Long> {
+    List<FeedbacksComplains> findByFeedbacks(Feedbacks feedbacks);
+    FeedbacksComplains findByFeedbacksAndUsers(Feedbacks feedbacks, Users users);
+}
