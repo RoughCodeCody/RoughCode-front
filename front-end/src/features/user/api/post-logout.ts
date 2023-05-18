15e4362c840e9e4c2955ab76ea0d346ae42b572e
유정훈 <choanury@naver.com>
2023-05-18 12:45:48 +0900

    fix(FE): #S08P31A306-347 resolve the issue where the logout does not redirect to the main page

--- front-end/src/features/user/api/post-logout.ts
+++ front-end/src/features/user/api/post-logout.ts
@@ -11,3 +11,23 @@ export const useLogout = () => {
     mutationFn: logout,
   });
 };
+
+// import { useMutation } from "@tanstack/react-query";
+
+// import { axios } from "@/lib/axios";
+// import { MutationConfig, queryClient } from "@/lib/react-query";
+
+// const logout = (): Promise<null> => {
+//   return axios.post(`/user/logout`);
+// };
+
+// type useLogoutOptions = {
+//   config?: MutationConfig<typeof logout>;
+// };
+
+// export const useLogout = ({ config }: useLogoutOptions = {}) => {
+//   return useMutation({
+//     mutationFn: logout,
+//     ...config,
+//   });
+// };
