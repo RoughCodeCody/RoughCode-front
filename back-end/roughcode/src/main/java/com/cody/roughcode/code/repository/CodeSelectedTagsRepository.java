bee142534ccdb25e9d6ff4da6538540575e11808
RyuJeongmin <jmyoo88@naver.com>
2023-05-19 01:22:08 +0900

    fix @Param import error

--- back-end/roughcode/src/main/java/com/cody/roughcode/code/repository/CodeSelectedTagsRepository.java
+++ back-end/roughcode/src/main/java/com/cody/roughcode/code/repository/CodeSelectedTagsRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import com.cody.roughcode.code.entity.Codes;
-import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
